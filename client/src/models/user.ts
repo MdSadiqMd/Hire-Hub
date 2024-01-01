00a2f418587dda6c4b9eb86457026e76535c9647
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-01 18:56:51 +0530

    refactor updated schema and added route

--- client/src/models/user.ts
+++ client/src/models/user.ts
@@ -1,8 +1,8 @@
 import { Schema, model, Document, models } from "mongoose";
 
-enum Designation {
-  EMPLOYEE = 'In Search of Job',
-  MANAGER = 'To Hire'
+export enum Designation {
+  EMPLOYEE = "In Search of Job",
+  MANAGER = "To Hire",
 }
 
 interface IUser extends Document {
@@ -12,11 +12,13 @@ interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  designation: { type: String, enum: Object.values(Designation), required: true }
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  designation: {
+    type: String,
+    enum: Object.values(Designation),
+    required: true,
+  },
 });
 
-const UserModel = models.User || model<IUser>('User', userSchema);
-
-export default UserModel;
+export default models.User || model<IUser>("User", userSchema);
