3f72a90c5566530e7e0a5719006d78dac4a7d514
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-08 18:24:40 +0530

    chore added dependencied

---
+++ client/pages/signup.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CreateAccount from '@/components/ui/create-account';
+
+const SignupPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      <CreateAccount />
+    </div>
+  );
+};
+
+export default SignupPage;
