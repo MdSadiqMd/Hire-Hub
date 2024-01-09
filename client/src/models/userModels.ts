7e100618957dac1fa9b65de04824fbd8b480a43a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-09 19:53:01 +0530

    refactor added extra feilds in schema

--- client/src/models/userModels.ts
+++ client/src/models/userModels.ts
@@ -1,33 +1,51 @@
-import mongoose from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-interface userInterface {
-  name: string;
+interface UserInterface extends Document {
+  username: string;
   email: string;
   password: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+  forgotPasswordToken?: string;
+  forgotPasswordTokenExpiry?: Date;
+  verifyToken?: string;
+  verifyTokenExpiry?: Date;
 }
 
-const schema = new mongoose.Schema<userInterface>({
-  name: {
+const userSchema = new Schema<UserInterface>({
+  username: {
     type: String,
-    required: true,
+    required: [true, "Please Provide a Username"],
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Please Provide an Email"],
     unique: true,
     match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Please Provide a Password"],
     validate: {
-      validator: (value: any) => value > 6,
-      message: "value must be greater than 6",
+      validator: (value: any) => value.length > 6,
+      message: "Password must be greater than 6 characters",
     },
   },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
 });
 
 const User =
-  mongoose.models.user || mongoose.model<userInterface>("user", schema);
+  mongoose.models.user || mongoose.model<UserInterface>("user", userSchema);
 
 export default User;
