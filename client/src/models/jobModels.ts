63c43e80e55c841e5066856c7488fd01f74f5b8e
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-19 20:33:26 +0530

    refactor added job schema

---
+++ client/src/models/jobModels.ts
@@ -0,0 +1,95 @@
+import { Document, Schema } from "mongoose";
+
+interface JobInterface extends Document {
+  title: string;
+  location: string;
+  skillsRequired: string[];
+  jobDesignation: string;
+  educationQualification: string;
+  experience: number;
+  freshersEligible: boolean;
+  isVerified: boolean;
+  isAvailable: boolean;
+  postedAt?: Date;
+  updatedAt?: Date;
+  online?: boolean;
+  workType?: boolean;
+  internship: boolean;
+}
+
+const jobSchema = new Schema<JobInterface>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: (value: string) => /^[a-zA-Z0-9\s]*$/.test(value),
+        message: "Title can only contain alphanumeric characters and spaces.",
+      },
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    skillsRequired: {
+      type: [String],
+      required: true,
+    },
+    jobDesignation: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    educationQualification: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    experience: {
+      type: Number,
+      required: true,
+    },
+    freshersEligible: {
+      type: Boolean,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    postedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+    },
+    online: {
+      type: Boolean,
+      default: false,
+    },
+    workType: {
+      type: Boolean,
+      default: true,
+    },
+    internship: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Job = mongoose.model<JobInterface>("Job", jobSchema);
+
+export default Job;
