4551671bad9574b9db14bda26c9f1cbc1d51ef66
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-25 20:31:56 +0530

    refactor added object fetching with id

--- client/src/app/api/job/route.ts
+++ client/src/app/api/job/route.ts
@@ -3,21 +3,32 @@ import connectDB from "@/db/config";
 import jobModel from "@/Models/jobModels";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(req: NextRequest, {params}) {
   try {
-    console.log("connecting");
-
+    console.log("Connecting to the database");
     await connectDB();
-    console.log("Connected");
-    const job = await jobModel.findById({ _id: `${request}` });
+    console.log("Connected to the database");
+    console.log(params.id)
 
+    const jobId: string = params.id as string;
+    console.log("Received jobId:", jobId);
+
+    // Validate that jobId is a valid ObjectId
+    if (!ObjectId.isValid(jobId)) {
+      console.log("Invalid job ID");
+      return NextResponse.json({ msg: "Invalid job ID" }, { status: 400 });
+    }
+
+    console.log("Valid job ID:", jobId);
+
+    const job = await jobModel.findOne({ id: ObjectId(jobId) });
     if (!job) {
       console.log("Job not found");
       return NextResponse.json({ msg: "Job not found" }, { status: 404 });
     }
 
-    console.log(job);
-    return NextResponse.json({ "result": job }, { status: 200 });
+    console.log("Job found:", job);
+    return NextResponse.json({ result: job }, { status: 200 });
   } catch (error) {
     console.error("Error fetching job:", error);
     return NextResponse.json({ msg: "Something went wrong" }, { status: 500 });
