25e72043cf932c268f67587603f1d162ecd7cb15
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-01-25 21:50:15 +0530

    refactor added object fetching with id

--- client/src/app/api/jobs/[id]/route.ts
+++ client/src/app/api/jobs/[id]/route.ts
@@ -3,9 +3,10 @@ import { ObjectId } from "mongodb";
 import connectDB from "@/db/config";
 import jobModel from "@/Models/jobModels";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
 // Define the handler function
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+/*export async function GET(req: NextApiRequest, res: NextApiResponse) {
   try {
     console.log("Connecting to the database");
     await connectDB();
@@ -38,5 +39,20 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
   } catch (error) {
     console.error("Error fetching job:", error);
     res.status(500).json({ msg: "Something went wrong" });
+  }
+}*/
+
+export async function GET(req:NextResponse, params:NextResponse) {
+  try {
+    console.log("Connecting to the database");
+    await connectDB();
+    console.log("Connected to the database");
+
+    const id = params;
+    const job = await jobModel.findById(id);
+    console.log("Job found:", job);
+    return NextResponse.json(job);
+  } catch (error) {
+    console.error("Error fetching job:", error);
   }
 }
