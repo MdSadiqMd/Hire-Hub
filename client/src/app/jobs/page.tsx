6dd96b5ae2174b223a819fcb1202fbc884adefc9
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-14 20:08:18 +0530

    refactor added loading logic

--- client/src/app/jobs/page.tsx
+++ client/src/app/jobs/page.tsx
@@ -41,9 +41,13 @@ interface PageProps {
 
 const Page: NextPage<PageProps> = ({ searchParams }) => {
   const [data, setData] = useState<JobData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
   const search = useSearchParams().get("search");
   const fetchData = async () => {
+    setLoading(true);
+    setError(null);
     try {
       console.log(search);
       const res = await axios.get("/api/jobs", { params: { search: search } });
@@ -55,12 +59,17 @@ const Page: NextPage<PageProps> = ({ searchParams }) => {
           setData(fetchedData);
         } else {
           console.error("Data is not an array:", fetchedData);
+          setError("Data is not an array");
         }
       } else {
         console.error("Failed to fetch data");
+        setError("Failed to fetch data");
       }
     } catch (error) {
       console.error("Error fetching data:", error);
+      setError("Error fetching data");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -108,70 +117,79 @@ const Page: NextPage<PageProps> = ({ searchParams }) => {
           </div>
         </div>
         <main className="mt-5 p-5 max-h-screen overflow-auto">
-          {data.map((job, i) => (
-            <div
-              key={i}
-              onClick={() => handleClick(job._id)}
-              style={{ cursor: "pointer" }}
-            >
-              <div className="group mx-2 mt-4 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto">
-                <Link
-                  href="#"
-                  passHref
-                  className="order-2 col-span-1 mt-4 -ml-14 text-left text-gray-600 hover:text-gray-700 sm:-order-1 sm:ml-4"
-                >
-                  <div className="group relative h-16 w-16 overflow-hidden rounded-lg">
-                    <img
-                      src={job.companyLogo ?? "/default-logo.png"} // Use a default value if companyLogo is null
-                      alt=""
-                      className="h-full w-full object-cover text-gray-700"
-                    />
-                  </div>
-                </Link>
-                <div className="col-span-11 flex flex-col pr-8 text-left sm:pl-4">
-                  <h3 className="text-sm text-gray-600">{job.companyName}</h3>
-                  <Link
-                    href="#"
-                    className="mb-3 overflow-hidden pr-7 text-lg font-semibold sm:text-xl"
+          {loading ? (
+            <h1>loading</h1>
+          ) : (
+            <div>
+              {data.map((job, i) => {
+                return (
+                  <div
+                    key={i}
+                    onClick={() => handleClick(job._id)}
+                    style={{ cursor: "pointer" }}
                   >
-                    {job.title}
-                  </Link>
-                  <p className="overflow-hidden pr-2 text-sm">
-                    {job.jobDescription}
-                  </p>
-                  <div className="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2">
-                    <div>
-                      Experience:
-                      <span className="ml-2 mr-3 rounded-full bg-green-100 px-2 py-0.5 text-green-900">
-                        {job.experience}
-                      </span>
+                    <div className="group mx-2 mt-4 grid max-w-screen-md grid-cols-12 space-x-8 overflow-hidden rounded-lg border py-8 text-gray-700 shadow transition hover:shadow-lg sm:mx-auto">
+                      <Link
+                        href="#"
+                        passHref
+                        className="order-2 col-span-1 mt-4 -ml-14 text-left text-gray-600 hover:text-gray-700 sm:-order-1 sm:ml-4"
+                      >
+                        <div className="group relative h-16 w-16 overflow-hidden rounded-lg">
+                          <img
+                            src={job.companyLogo ?? "/default-logo.png"} // Use a default value if companyLogo is null
+                            alt=""
+                            className="h-full w-full object-cover text-gray-700" />
+                        </div>
+                      </Link>
+                      <div className="col-span-11 flex flex-col pr-8 text-left sm:pl-4">
+                        <h3 className="text-sm text-gray-600">
+                          {job.companyName}
+                        </h3>
+                        <Link
+                          href="#"
+                          className="mb-3 overflow-hidden pr-7 text-lg font-semibold sm:text-xl"
+                        >
+                          {job.title}
+                        </Link>
+                        <p className="overflow-hidden pr-2 text-sm">
+                          {job.jobDescription}
+                        </p>
+                        <div className="mt-5 flex flex-col space-y-3 text-sm font-medium text-gray-500 sm:flex-row sm:items-center sm:space-y-0 sm:space-x-2">
+                          <div>
+                            Experience:
+                            <span className="ml-2 mr-3 rounded-full bg-green-100 px-2 py-0.5 text-green-900">
+                              {job.experience}
+                            </span>
+                          </div>
+                          <div>
+                            Salary:
+                            <span className="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900">
+                              {formatSalaryRange(job.salary)}
+                            </span>
+                          </div>
+                        </div>
+                        <div>
+                          <h5 className="text-white space-x-2 mt-2">
+                            {job.skillsRequired.map((skill, index) => (
+                              <Badge key={index} variant="outline">
+                                {skill}
+                              </Badge>
+                            ))}
+                          </h5>
+                        </div>
+                        <div className="flex mt-2 items-center text-sm font-medium text-gray-500">
+                          <p>
+                            {job.updatedAt ? "Updated At: " : "Published At: "}
+                            {formatDate(job.updatedAt ?? job.postedAt)}
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                    <div>
-                      Salary:
-                      <span className="ml-2 mr-3 rounded-full bg-blue-100 px-2 py-0.5 text-blue-900">
-                        {formatSalaryRange(job.salary)}
-                      </span>
-                    </div>
                   </div>
-                  <div>
-                    <h5 className="text-white space-x-2 mt-2">
-                      {job.skillsRequired.map((skill, index) => (
-                        <Badge key={index} variant="outline">
-                          {skill}
-                        </Badge>
-                      ))}
-                    </h5>
-                  </div>
-                  <div className="flex mt-2 items-center text-sm font-medium text-gray-500">
-                    <p>
-                      {job.updatedAt ? "Updated At: " : "Published At: "}
-                      {formatDate(job.updatedAt ?? job.postedAt)}
-                    </p>
-                  </div>
-                </div>
-              </div>
+                );
+              })}
             </div>
-          ))}
+          )}
         </main>
       </div>
     </>
