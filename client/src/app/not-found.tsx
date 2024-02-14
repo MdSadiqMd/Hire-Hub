b9f9b85d59d2d50b83229af1e0be98f5b5c54e43
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-14 21:41:53 +0530

    refactor added not found page

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NotFound() {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center min-h-screen mt-7">
+        <Link href="/home" className="font-serif font-medium text-2xl">
+          Return <span className="text-blue-600">Home</span>
+        </Link>
+        <Image
+          src="/not-found.svg"
+          alt="mockup"
+          width={2000}
+          height={2000}
+          className="w-full md:w-auto max-w-2xl mx-auto"
+        />
+      </div>
+    </div>
+  );
+}
