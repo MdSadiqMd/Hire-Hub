f8cfae9c0d8f6bfb5c4b096afe4f87e002354dd6
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-18 19:16:52 +0530

    refactor chened from if else to switch case statement

--- client/src/app/api/jobs/route.ts
+++ client/src/app/api/jobs/route.ts
@@ -17,44 +17,44 @@ export async function GET(req: NextRequest) {
     console.log("connecting MongoDB");
     await connectDB();
     console.log("MongoDB connected");
-    if (workType.length-1 > 0 && workType!=='') {
-      console.log("Aggregation Pipeline Filter");
-      const result = await jobModel.aggregate([
-        {
-          $search: {
-            index: "text-search",
-            text: {
-              query: workType,
-              path: {
-                wildcard: "*",
+    let result;
+    switch (true) {
+      case workType && workType !== "":
+        console.log("Aggregation Pipeline Filter");
+        result = await jobModel.aggregate([
+          {
+            $search: {
+              index: "text-search",
+              text: {
+                query: workType,
+                path: {
+                  wildcard: "*",
+                },
               },
             },
           },
-        },
-      ]);
-      //console.log(result);
-      return NextResponse.json({ result }, { status: 200 });
-    } else if (query && query !== "null") {
-      console.log("Aggregation Pipeline");
-      const result = await jobModel.aggregate([
-        {
-          $search: {
-            index: "text-search",
-            text: {
-              query: query,
-              path: {
-                wildcard: "*",
+        ]);
+        break;
+      case query && query !== "null":
+        console.log("Aggregation Pipeline");
+        result = await jobModel.aggregate([
+          {
+            $search: {
+              index: "text-search",
+              text: {
+                query: query,
+                path: {
+                  wildcard: "*",
+                },
               },
             },
           },
-        },
-      ]);
-      //console.log(result);
-      return NextResponse.json({ result }, { status: 200 });
-    } else {
-      const result = await jobModel.find({});
-      return NextResponse.json({ result }, { status: 200 });
+        ]);
+        break;
+      default:
+        result = await jobModel.find({});
     }
+    return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ msg: "connection failed" }, { status: 400 });
   }
