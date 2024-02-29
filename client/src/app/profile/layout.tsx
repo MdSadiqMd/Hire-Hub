33dad126a06571170c888f87cd64cfd0b40cc48a
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-02-29 12:38:32 +0530

    added links in nav bar

--- client/src/app/profile/layout.tsx
+++ client/src/app/profile/layout.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { SidebarNav } from "@/components/ui/sidebar-nav";
+import { Navbar } from "@/components/ui/navbar";
 
 const sidebarNavItems = [
   {
@@ -22,16 +23,19 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
-      <div className="space-y-6 p-10 pb-16">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
+      <div>
+        <Navbar search={children.search} />
+        <div className="space-y-6 p-10 pb-16">
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+            <aside className="-mx-4 lg:w-1/5">
+              <SidebarNav items={sidebarNavItems} />
+            </aside>
+            <div className="flex-1 lg:max-w-2xl">{children}</div>
+          </div>
         </div>
       </div>
     </>
