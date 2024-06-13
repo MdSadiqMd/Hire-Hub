141603c94f4c8b1b013692be3e1186768e0b1654
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-06-13 16:03:57 +0530

    debug import error

--- client/src/app/signup/page.tsx
+++ client/src/app/signup/page.tsx
@@ -20,7 +20,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { signIn } from "next-auth/react";
 import { useSession } from "next-auth/react";
-import User from "@/Models/userModels";
+import User from '@/Models/userModels'
 
 export default function SignupPage() {
   const router = useRouter();
