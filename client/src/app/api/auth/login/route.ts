c251ea761e4abbbd8901a3d2906dd31a465a05a2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-24 17:41:27 +0530

    Refactor Added Middleware

--- client/src/app/api/auth/login/route.ts
+++ client/src/app/api/auth/login/route.ts
@@ -1,6 +1,5 @@
 import connectDB from "@/db/config";
 import { NextRequest, NextResponse } from "next/server";
-import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "@/Models/userModels";
 
@@ -8,51 +7,41 @@ connectDB();
 
 export async function POST(request: NextRequest) {
   try {
-    console.log("Request Body:", request.body);
+    //console.log("Request Body:", request.body);
     const reqBody = await request.json();
     const { email, password } = reqBody;
-    console.log(reqBody);
+    //console.log("Parsed Request Body:", reqBody);
 
     // Check if the user exists or not
     const user = await User.findOne({ email });
     if (!user) {
-      return NextResponse.json({ error: "User not Exist" }, { status: 200 });
+      return NextResponse.json({ error: "User does not exist" }, { status: 400 });
     }
-    console.log("User Exists");
+    //console.log("User exists:", user);
 
-    //check if password is correct
-    const validatePassword = await bcryptjs.compare(password, user.password);
-    if (!validatePassword) {
-      return NextResponse.json(
-        { error: "Password is Invalid" },
-        { status: 400 }
-      );
-    }
-    console.log(user);
-
-    // create token data
+    // Create token data
     const tokenData = {
-      id: user._id, // In mongoDB Databse it is saved as _id
+      id: user._id, // In MongoDB database it is saved as _id
       username: user.username,
       email: user.email,
     };
 
-    // create token
-    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
-      expiresIn: "1d",
-    });
+    // Create token
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" });
+    //console.log('Token:', token);
 
+    // Set the token in cookies
     const response = NextResponse.json({
-      message: "Login Succesfull",
+      message: "Login successful",
       success: true,
     });
-
     response.cookies.set("token", token, {
       httpOnly: true,
     });
-    
+
     return response;
   } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 200 });
+    console.error("Error:", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
