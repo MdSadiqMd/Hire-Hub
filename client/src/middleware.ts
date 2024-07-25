abb972ee3b495ac60a37b0accf96e8a92c1070dc
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-07-25 15:25:26 +0530

    Refactor modified matcher in middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -6,13 +6,23 @@ export function middleware(request: NextRequest) {
     const publicPaths = ["/login", "/signup", '/privacy', '/terms'];
     const isPublicPath = publicPaths.includes(path);
     const token = request.cookies.get("token")?.value || "";
+
     if (!isPublicPath && !token) {
         return NextResponse.redirect(new URL("/login", request.nextUrl));
     }
+
     if (isPublicPath && token) {
         return NextResponse.redirect(new URL("/jobs", request.nextUrl));
     }
+    return NextResponse.next();
 }
+
 export const config = {
-    matcher: ["/", "/login", "/signup", "/jobs", "/profile"],
+    matcher: [
+        "/profile/:path*",
+        "/jobs/:path*",
+        "/",
+        "/login",
+        "/signup"
+    ],
 };
